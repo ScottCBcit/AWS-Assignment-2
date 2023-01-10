bf6449386799df08a0a2f29e72357764257ac27a
Scott Croin <scroin@my.bcit.ca>
2023-01-10 13:54:43 -0800

    Sql port config

--- mysqlDatabase.js
+++ mysqlDatabase.js
@@ -8,6 +8,7 @@ const pool = mysql
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306,
     waitForConnections: true,
   })
   .promise()
